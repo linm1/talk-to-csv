6719be7f34e052d3ce4ba7e6d6e18a1db8b4013f
linm1 <142962001+linm1@users.noreply.github.com>
2023-12-28 07:31:50 +0000

    main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from langchain_experimental.agents.agent_toolkits import create_csv_agent
+from langchain.llms import VertexAI
+import os
+
+os.environ[
+    "GOOGLE_APPLICATION_CREDENTIALS"] = r'C:\Users\kllkt\Downloads\norse-carport-257701-eff34531b53d.json'
+
+
+st.set_page_config(page_title="CSV Agent", page_icon=":robot_face:")
+
+def main():
+  user_csv = st.file_uploader("Upload your CSV file", type="csv")
+  if user_csv is not None:
+    user_question = st.text_input("Ask your question about the CSV file")
+    if user_question:
+      llm = VertexAI(model_name="gemini-pro", temperature=0)
+      agent = create_csv_agent(llm, user_csv, verbose=True)
+      with st.spinner("Thinking..."):
+        response = agent.run(user_question)
+      st.write(response)
+
+
+if __name__ == "__main__":
+  main()
